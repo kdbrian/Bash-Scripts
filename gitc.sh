d137fcd342452af769e55e164117a031835fda1b
junrdev <juniorprogrammer09@gmail.com>
2023-03-16 15:25:30 +0300

    added functionality for dir and file check

--- gitc.sh
+++ gitc.sh
@@ -9,17 +9,33 @@ echo
 
 echo "Git commit client"
 
-#check if the file name and commit message are give
-if [[ $# -lt 2 ]]; then
-	echo "Error in $0"
-	echo "Usage: $0 <filename> <commit message>"
-	exit 1
+#check if git is initialized in the working directory
+#check if we have a .git directory
+if [[ -d .git ]]; then
+
+	#check if the file name and commit message are give
+	if [[ $# -lt 2 ]]; then
+		echo "Error in $0"
+		echo "Usage: $0 <filename> <commit message>"
+		exit 1
+	fi
+
+	#check if the first parameter is actually a file and not a directory
+	if [[ -f $1 ]]; then
+
+		#  stage and commit the file
+		echo "Staging and commiting $1......"
+
+		git add $1 && git commit -m "$2"
+	else
+		echo "Error: the parameter provided is not a file"
+		echo "Usage: $0 <filename> <commit message>"
+	fi
+
+else
+	echo "Error: No existing git repository in the current directory"
+	exit 0
 fi
-
-#  stage and commit the file
-echo "Staging and commiting $1......"
-
-git add $1 && git commit -m "$2"
 
 echo
 
