3845d4ad87d7975ace6cad0c028636b13dc41afb
junrdev <juniorprogrammer09@gmail.com>
2023-03-16 20:38:16 +0300

    if statements final

--- ifst.sh
+++ ifst.sh
@@ -4,3 +4,13 @@
 
 # start with if  end with fi
 #works by checking the exit status of a command
+
+
+if [ 2 -eq 1 ]; then
+	echo "test 1 passed"
+
+elif [ 2 -gt 1 ]; then
+	echo "test 2 passed"
+else
+	echo test failed
+fi
