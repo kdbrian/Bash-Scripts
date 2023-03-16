d757216ee0c685ae8c58cc9378b9c44a314f4c91
junrdev <juniorprogrammer09@gmail.com>
2023-03-16 21:09:51 +0300

    if statements final modified

--- ifst.sh
+++ ifst.sh
@@ -14,3 +14,18 @@ elif [ 2 -gt 1 ]; then
 else
 	echo test failed
 fi
+
+#more logic
+a=$(cat file1.txt)
+b=$(cat file2.txt)
+c=$(cat file3.txt)
+
+# if test && test2; then
+#or
+# if test1 || test2; then
+if [ "$a" = "$b" ] && [ "$a" = "$c" ]; then
+	echo "removed two kept 1"
+	rm file2.txt file3.txt;
+else
+	echo "different keep all"
+fi
