6bb19ea4e713e4ddb5b4cc6a7fbc6d02584674da
junrdev <juniorprogrammer09@gmail.com>
2023-03-16 21:12:38 +0300

    memory logger project modified

--- memlog.sh
+++ memlog.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-dir=$HOME/performance
-
+# creates a log file of the memory usage of the current logged in user in mem.logs file
+# starts by checking if the folder exists if not creates and the creates the logs files
 if [ -d ~/performance ]; then
 	echo "Folder exists"
 	echo "Logs : $(date)" >> ~/performance/mem.logs
-	free >> ~/performance/mem.logs
 else
 	echo "creating folder"
 	mkdir ~/performance
 	echo "Logs created on : $(date)" >> ~/performance/mem.logs
-	free >> ~/performance/mem.logs
 fi
+
+free >> ~/performance/mem.logs
