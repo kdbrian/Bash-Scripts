f528f6fddad8c7cd981ce09e152fed6c2dc9486b
junrdev <juniorprogrammer09@gmail.com>
2023-03-21 00:42:02 +0300

    the fork system call

---
+++ SystemsProgramming/fork.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+// emulation of creating a child process  using the fork command
+
+size_t pid;
+
+int main(){
+
+	printf("before fork\n");
+
+
+	pid= fork();
+
+	if(pid == 0){
+		printf("Child process pid : %d\n", getpid());
+		printf("Parent pid : %d\n", getppid());
+	}else if( pid > 0){
+		printf("Parent process pid : %d\n", getpid());
+		printf("Child process' pid : %d\n", pid);
+	}
+
+	printf("Outside fork \n");
+return 0;
+}
