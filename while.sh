6393380e0f899ba02d71b7da3085d40a2c2f3afe
junrdev <juniorprogrammer09@gmail.com>
2023-03-20 22:39:45 +0300

    while loops final

--- while.sh
+++ while.sh
@@ -1,1 +1,16 @@
 #! /bin/bash
+
+
+# while loops
+
+# runs a certain command while the exit status is zero(0)
+
+# syntax while ... done
+
+read -p "Enter a number" num
+
+while [[ $num -gt 10 ]];do
+
+	echo $num;
+	num=$(( $num-1 ))
+done
