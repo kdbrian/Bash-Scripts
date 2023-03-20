8e211b6e8ba6d7c60cf5111e9395cd37743c9bdc
junrdev <juniorprogrammer09@gmail.com>
2023-03-21 00:32:41 +0300

    debug some error messages

--- getops.sh
+++ getops.sh
@@ -10,6 +10,8 @@
 
 # creating a temp converter from farenheit to celcius and vice versa
 
+# usage ./getopts.sh -f|-c <value>
+
 # getops "f:c:" to ensure that it gets all the options we nest it inside a while loop condition
 
 # the argument we put after the option gets saved in the $OPTARG variable
@@ -19,14 +21,15 @@ while getopts "f:c:" opt ; do
 	#checking the user options using case statements
 	case "$opt" in
 		#farenheit to celcius
-		f) result= $(echo "scale=2; ($OPTARG - 32) * (5 / 9)" | bc) ;;
+		f) result=$(echo "scale=2; ($OPTARG - 32) * (5 / 9)" | bc) ;;
 
 		#celcius to farenheit
-		c) result= $(echo "scale=2; ($OPTARG * (9 / 5)) + 32" | bc) ;;
+		c) result=$(echo "scale=2; ($OPTARG * (9 / 5)) + 32" | bc) ;;
 
 		#any other unusual option since the getopts will provide a ? if no option is given
 		\?) echo "Error in $0"; echo "Usage $0 [-f|-c]" ;;
 	esac
-done
 
 echo $result
+
+done
