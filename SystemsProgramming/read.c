5803a57cefe8432ba3d29cafd6642cb9874fb493
junrdev <juniorprogrammer09@gmail.com>
2023-03-22 17:12:33 +0300

    the read system call

---
+++ SystemsProgramming/read.c
@@ -0,0 +1,50 @@
+#include "headers.h"
+
+/**
+    The read system call
+    read a number of characters from a file descriptor
+*/
+
+char Buf[30];
+
+/***
+    Read data from the terminal using the read command and writes the output to stdout
+*/
+void readTerminal(char *data){
+
+    read(0, data, 10);
+    printf("Reading finished\n");
+    printf("Read : {%s}\n", data);
+}
+
+/***
+    Read data from a file using the read command and writes the output to stdout
+*/
+void readFile(char * data){
+
+    int fd = open("file1.txt", O_CREAT, 0666);
+
+    read(fd, data, 30);
+
+    printf("Read : [%s]\n", data);
+}
+
+/***
+    flushes the char array by initializing each index to the terminating character '\0
+*/
+void flushArray(char * arry){
+
+    for (size_t i=0; i< sizeof(arry); i++)
+        arry[i]='\0';
+}
+
+int main(){
+
+    readFile(Buf);
+
+    flushArray(Buf);
+
+    readTerminal(Buf);
+
+    return 0;
+}
